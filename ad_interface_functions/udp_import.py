5acc306c9b20e2b354dc4dfb24b9af0ea59a5189
Alexander Heilmeier <heilmeier@ftm.mw.tum.de>
2019-06-07 07:52:12 +0200

    Updated udp_import with a buffer possibility

--- ad_interface_functions/udp_import.py
+++ ad_interface_functions/udp_import.py
@@ -1,13 +1,15 @@
 import socket
+from typing import Union
 
 
-def udp_import(sock: socket.socket, data_size: int) -> bytes:
+def udp_import(sock: socket.socket, data_size: int, use_buffer: bool = False) -> Union[bytes, list]:
     """
     Created by:
     Alexander Heilmeier
 
     Documentation:
-    Import data via UDP. data_size in bytes!
+    Import data via UDP. data_size in bytes! use_buffer flag can be used to return the complete buffer instead of just
+    returning the latest message.
     """
 
     # ------------------------------------------------------------------------------------------------------------------
@@ -15,15 +17,22 @@ def udp_import(sock: socket.socket, data_size: int) -> bytes:
     # ------------------------------------------------------------------------------------------------------------------
 
     data_b = None
+    buffer = []
 
     try:
         while True:
             data_b, addr = sock.recvfrom(data_size)
 
+            if use_buffer:
+                buffer.append(data_b)
+
     except socket.error:
         pass
 
-    return data_b
+    if use_buffer:
+        return buffer
+    else:
+        return data_b
 
 
 # testing --------------------------------------------------------------------------------------------------------------
