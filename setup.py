69bd0f3efcc93c036d953cd34feabe1f56358e6b
Alexander Heilmeier <alexander.heilmeier@tum.de>
2020-09-11 13:28:32 +0200

    Updated to 0.20

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ad-interface-functions',
-    version='0.19',
+    version='0.20',
     url='https://www.ftm.mw.tum.de/en/main-research/vehicle-dynamics-and-control-systems/roborace-autonomous-motorsport/',
     author="Alexander Heilmeier, Tim Stahl",
     author_email="alexander.heilmeier@tum.de, stahl@ftm.mw.tum.de",
@@ -20,13 +20,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=[
-        'pyzmq==18.1.1'
+        'pyzmq==19.0.1'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent",
     ])
